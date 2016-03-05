fe37c0a2506b1830070514a40e99967d1b2b15cd
Clarke <clarke@Clarkes-Mac.local>
2016-03-05 00:07:46 -0500

    Removed console.logs, added missing semicolons

--- public/assignment/views/header/header.controller.js
+++ public/assignment/views/header/header.controller.js
@@ -8,6 +8,6 @@ angular
 function HeaderController($scope, $location, $rootScope, UserService) {
     $scope.logout = function(){
         $rootScope.user = null;
-        $location.path('/home')
+        $location.path('/home');
     }
 }
--- public/assignment/views/main/main.controller.js
+++ public/assignment/views/main/main.controller.js
@@ -6,21 +6,18 @@ angular
 
 
 function MainController($scope, $location, $rootScope) {
-    console.log("Main Controller")
-
     // Dynamically set $scope.path to friendly version of hash
     // // Note: Attached to rootScope here because it is used in multiple views
     $rootScope.path = FormatPath($location.url())
     $scope.$on('$locationChangeStart', (function(){
-        $rootScope.path = FormatPath($location.url())
+        $rootScope.path = FormatPath($location.url());
     }));
 }
 
 function FormatPath(path){
-    var loc = path.lastIndexOf('/')
+    var loc = path.lastIndexOf('/');
     if (loc != -1){
         path = path.slice(loc+1);
     }
-    console.log(path);
     return path;
 }
