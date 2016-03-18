2efa78d1d738de75b555fa41be97b23b490a6fb6
Clarke <clarke@Clarkes-Mac.local>
2016-03-18 19:31:00 -0400

    User Service

--- public/assignment/client/services/user.service.client.js
+++ public/assignment/client/services/user.service.client.js
@@ -1,65 +1,29 @@
 angular
     .module('FormBuilderApp')
-    .factory('UserService', function(){
+    .factory('UserService', function($http){
 
     var fac = {};
 
-        fac.users = [
-            {        "_id":123, "firstName":"Alice",            "lastName":"Wonderland",
-                "username":"alice",  "password":"alice",   "roles": ["student"]                },
-            {        "_id":234, "firstName":"Bob",              "lastName":"Hope",
-                "username":"bob",    "password":"bob",     "roles": ["admin"]                },
-            {        "_id":345, "firstName":"Charlie",          "lastName":"Brown",
-                "username":"charlie","password":"charlie", "roles": ["faculty"]                },
-            {        "_id":456, "firstName":"Dan",              "lastName":"Craig",
-                "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"]},
-            {        "_id":567, "firstName":"Edward",           "lastName":"Norton",
-                "username":"ed",     "password":"ed",      "roles": ["student"]                }
-        ];
         fac.findUserByUsername = function(username){
-            for (var idx = 0; idx < fac.users.length; idx++){
-                if (fac.users[idx].username == username){
-                    return fac.users[idx];
-                }
-            }
-            return null;
+            return $http.get("/api/assignment/user?username=" + username);
         };
         fac.findUserByCredentials = function(username, password, callback){
-            var user = fac.findUserByUsername(username);
-            if (user && user.password == password){
-                return callback(user);
-            }
-            return callback(null);
+            return callback($http.get("api/assignment/user?username=" + username + "&password=" + password));
         };
         fac.findAllUsers = function(callback){
-            return callback(fac.users);
+            return callback($http.get("/api/assignment/user"));
         };
         fac.createUser = function(user, callback){
             if (fac.findUserByUsername(user.username)){
                 return callback(null);
             }
-            user._id = (new Date).getTime();
-            fac.users.push(user);
-            return callback(user);
+            return callback($http.post("/api/assignment/user", {body: user}));
         };
         fac.deleteUserById = function(userId, callback){
-            for (var idx = 0; idx < fac.users.length; idx++){
-                if (fac.users[idx]._id == userId){
-                    fac.users.splice(idx, 1);
-                    return callback(fac.users);
-                }
-            }
-            return callback(fac.users);
+            return callback($http.delete("/api/assignment/user?id=" + userId));
         };
         fac.updateUser = function(userId, user, callback){
-            user._id = userId;
-            for (var idx = 0; idx < fac.users.length; idx++){
-                if (fac.users[idx]._id == userId){
-                    fac.users[idx] = user;
-                    return callback(user);
-                }
-            }
-            return callback(null);
+            return callback($http.put("/api/assignment/user?id=" + userId, {body: user}));
         };
 
         return fac;
