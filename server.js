f54b9d123c539194f4189966849d555ccaecc79a
Clarke <clarke@Clarkes-Mac.local>
2016-03-25 01:16:07 -0400

    Fixed Server to Properly handle body for PUT, now puts client on root localhost

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
 
+// Body Parser: Needed for API Put
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Client
+app.use(express.static(__dirname + '/public/assignment/client/'));
+// Server
 require("./public/assignment/server/app.js")(app);
-
-app.use(express.static(__dirname + '/public'));
-
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+
 
 app.listen(port, ipaddress);
