212be379448ceef119eab10a99dc6cea6596b25a
Clarke <clarke@Clarkes-Mac.local>
2016-04-19 20:45:34 -0400

    Made home page look much better on mobile

--- public/project/client/views/home/home.controller.js
+++ public/project/client/views/home/home.controller.js
@@ -7,6 +7,20 @@ angular
 
 function HomeController($scope, $rootScope, ModalService) {
 
+    // For swapping active nav when you hit the arrow
+    $scope.active = function(lett){
+        if (!$scope.section){
+            if (lett == 'w'){
+                return 'active'
+            }
+        } else {
+            if ($scope.section[0] == lett){
+                return 'active';
+            }
+        }
+        return '';
+    }
+
     $scope.toggle = function(section){
         $scope.section = section;
     }
