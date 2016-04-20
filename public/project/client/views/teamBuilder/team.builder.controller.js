465d2c381852b95f7824231cdcdc8e95c4fbc98b
Clarke <clarke@Clarkes-Mac.local>
2016-04-20 01:36:06 -0400

    Remade the scaffolding on champ page and now it collapses cleanly on any layout. Also made modals better

--- public/project/client/views/teamBuilder/team.builder.controller.js
+++ public/project/client/views/teamBuilder/team.builder.controller.js
@@ -28,6 +28,8 @@ function TeamBuilderController($scope, $rootScope, ModalService, SummonerService
     };
 
     $scope.searchSummoner = function(idx){
+        console.log(idx);
+        console.log($scope.names);
         SummonerService.searchSummoner($scope.names[idx], idx);
     };
 
@@ -39,6 +41,28 @@ function TeamBuilderController($scope, $rootScope, ModalService, SummonerService
     $scope.updateRole = function(idx, to){
         SummonerService.updateRole(idx, to);
     };
+
+    // Close the modal without updating anything
+    $scope.close = function(){
+        ModalService.closeModal('summoner');
+    };
+
+    // Open the modal
+    $scope.openModal = function(idx){
+        console.log('opening: ' + idx);
+        $scope.idx = idx;
+        ModalService.openModal('summoner');
+    };
+
+    // Whether or not this modal should be open
+    var modalStatus = function(){
+        console.log(ModalService.modals);
+        $scope.open = ModalService.modals['summoner'];
+    };
+    // Listen for change in modal status
+    ModalService.listen(modalStatus);
+
+    modalStatus();
 
     updateData();
 
